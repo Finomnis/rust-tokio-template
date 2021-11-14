6e4902a8e9f3d1cc1eb2df4a9d987fdfe94c8102
Finomnis <finomnis@gmail.com>
2021-11-14 11:58:49 +0100

    Update template

--- asldkfj/src/main.rs
+++ asldkfj/src/main.rs
@@ -1,10 +1,10 @@
 mod command_line;
 mod dummy_task;
-mod shutdown_trigger;
+mod graceful_shutdown;
 
 use anyhow::Result;
+use graceful_shutdown::{start_submodule, wait_until_shutdown};
 use log;
-use shutdown_trigger::{start_submodule, shutdown_with_timeout};
 use tokio::time::Duration;
 
 #[tokio::main]
@@ -13,21 +13,18 @@ async fn main() -> Result<()> {
     let _opts = command_line::parse();
 
     // Register Ctrl+C and SIGTERM handlers
-    shutdown_trigger::register_signal_handlers();
+    graceful_shutdown::register_signal_handlers();
 
     // Actual program
     log::info!("Hello, world!");
     let dummy_task_handle = start_submodule(dummy_task::dummy_task());
 
-    // Wait for signals
-    shutdown_trigger::wait_for_shutdown().await;
+    // Wait for program shutdown initiation
+    wait_until_shutdown().await;
 
-    // Shutdown procedure
-//     shutdown_with_timeout(Duration::from_secs(5),
-//         dummy_task_handle
-// );
-
-    log::info!("Shutting down ...");
-    log::info!("Shutdown successful.");
-    Ok(())
+    // Wait until all submodules have shut down
+    shutdown_with_timeout!(
+        Duration::from_millis(1000),
+        dummy_task_handle
+    )
 }
