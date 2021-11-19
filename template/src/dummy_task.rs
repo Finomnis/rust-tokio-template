630038e3282b68297e67247b02c36d728aeb4a0f
Finomnis <finomnis@gmail.com>
2021-11-19 19:45:59 +0100

    Update to tokio-graceful-shutdown 0.2.0

--- template/src/dummy_task.rs
+++ template/src/dummy_task.rs
@@ -1,13 +1,16 @@
 use anyhow::Result;
-use log;
-use tokio_graceful_shutdown::wait_until_shutdown_started;
+use async_trait::async_trait;
+use tokio_graceful_shutdown::{AsyncSubsystem, SubsystemHandle};
 
-pub async fn dummy_task() -> Result<()> {
-    log::info!("dummy_task started.");
+pub struct DummyTask {}
 
-    wait_until_shutdown_started().await;
+#[async_trait]
+impl AsyncSubsystem for DummyTask {
+    async fn run(&mut self, subsys: SubsystemHandle) -> Result<()> {
+        log::info!("dummy_task started.");
+        subsys.on_shutdown_requested().await;
+        log::info!("dummy_task stopped");
 
-    log::info!("dummy_task stopped");
-
-    Ok(())
+        Ok(())
+    }
 }
--- template/src/main.rs
+++ template/src/main.rs
@@ -2,28 +2,18 @@ mod command_line;
 mod dummy_task;
 
 use anyhow::Result;
-use log;
 use tokio::time::Duration;
-use tokio_graceful_shutdown::{
-    register_signal_handlers, start_submodule, wait_for_submodule_shutdown,
-    wait_until_shutdown_started,
-};
+use tokio_graceful_shutdown::Toplevel;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // Query command line options and initialize logging
     let _opts = command_line::parse();
 
-    // Register Ctrl+C and SIGTERM handlers
-    register_signal_handlers();
-
-    // Actual program
-    log::info!("Hello, world!");
-    let dummy_task_handle = start_submodule(dummy_task::dummy_task());
-
-    // Wait for program shutdown initiation
-    wait_until_shutdown_started().await;
-
-    // Wait until all submodules have shut down
-    wait_for_submodule_shutdown!(Duration::from_millis(1000), dummy_task_handle)
+    // Initialize and run subsystems
+    Toplevel::new()
+        .start("dummy_task", dummy_task::DummyTask {})
+        .catch_signals()
+        .wait_for_shutdown(Duration::from_millis(1000))
+        .await
 }
